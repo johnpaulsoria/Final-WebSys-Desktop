9853b1e732e2397f190736e78a8208f5932fd494
John Paul Soria <112761565+johnpaulsoria@users.noreply.github.com>
2023-04-20 14:10:59 +0700

    Add files via upload

---
+++ JS/reservation_script.js
@@ -0,0 +1,20 @@
+function submitData() {
+  const fullName = document.getElementById("fullName").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const emailAdd = document.getElementById("emailAdd").value;
+  const address = document.getElementById("address").value;
+  const destiAdd = document.getElementById("destiAdd").value;
+  const flightSched = document.getElementById("flightSched").value;
+  const pay = document.getElementById("pay").value;
+
+
+  console.log(`Name: ${fullName}`);
+  console.log(`Phone: ${phoneNumber}`);
+  console.log(`Email: ${emailAdd}`);
+  console.log(`Address: ${address}`);
+  console.log(`Destination Address: ${destiAdd}`);
+  console.log(`Flight Schedule: ${flightSched}`);
+  console.log(`Payment Method: ${pay}`);
+  console.log("");
+  alert("Success");
+}
